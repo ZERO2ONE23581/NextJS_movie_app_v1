be8ceab17c8bd8528b268e506047b8ea8238ae4a
aiden <zero2one23581@gmail.com>
2022-03-08 22:08:37 +0900

    server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -42,4 +34,18 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+/* BACK END */
+//getServerSideProps 이름철자 매우중요함!!
+//Whatever code you put inside here, it will only run on SERVER not CLINET SIDE (FRONT)!
+//결국 이기능의 본질은 api통신을 프론트쪽에서 감추고 오직 서버쪽에서만 일어나게 하는것
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  // you can put any data here and give it as props to the Component (Home) => then it goes to _app to run the {...pageProps}
+  return {
+    props: {
+      results,
+    },
+  };
 }
