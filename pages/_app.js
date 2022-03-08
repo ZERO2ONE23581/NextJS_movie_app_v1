81541b0ad0767576a54b5c7dfd07f1d37e43b10d
aiden <zero2one23581@gmail.com>
2022-03-08 19:52:49 +0900

    patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,10 @@
-import NavBar from "../components/NavBar";
-
-//global css를 쓸때는 반드시 _app.js파일에서 import 하도록 한다!
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <footer>
-        <small>made by Junwoo</small>
-      </footer>
-      <style jsx global>{`
-        a {
-          color: white;
-          padding: 5px;
-          text-decoration: none;
-        }
-      `}</style>
-    </div>
+    </Layout>
   );
 }
